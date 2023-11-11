4aedc47038f1e20398b5d8fa8850e12ab2028987
Mohammadreza <shahbaziweb@gmail.com>
2023-11-11 15:42:39 +0330

    feat: create project with vite

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // resolve: {
+  //   alias: {
+  //     "@assets": resolve(__dirname, "./src/assets/"),
+  //     "@components": resolve(__dirname, "./src/components/"),
+  //     "@pages": resolve(__dirname, "./src/pages/"),
+  //     "@services": resolve(__dirname, "./src/services/"),
+  //     "@styles-engine": resolve(__dirname, "./src/styles-engine/"),
+  //     "@forms-engine": resolve(__dirname, "./src/forms-engine/"),
+  //     "@models": resolve(__dirname, "./src/models/"),
+  //   }
+  // }
+})
