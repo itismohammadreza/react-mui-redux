bd416098d39003fcedd9656fafdd185e6b6f794d
mohammadreza <shahbaziweb@gmail.com>
2024-05-21 16:26:30 +0330

    feat: show global toast on requests

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,6 +3,7 @@ import { router } from "@root/router";
 import { StoreProvider } from "@redux/StoreProvider";
 import { ThemeProvider } from "@theme/theme";
 import { Loading } from "@components/Loading";
+import { Toast } from "@components/Toast";
 import '@locales/i18n';
 
 export const App = () => {
@@ -12,6 +13,7 @@ export const App = () => {
           <RouterProvider router={router}/>
           <Outlet/>
           <Loading/>
+          <Toast/>
         </ThemeProvider>
       </StoreProvider>
   )
